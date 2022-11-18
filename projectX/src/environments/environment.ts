2d135b883373c96f1c2e7746e06182efeb602f44
DKFAEL <rafaelnunesemp@hotmail.com>
2022-11-18 17:19:44 -0300

    Formulario de loguin com autenticação / Firebase

--- projectX/src/environments/environment.ts
+++ projectX/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyATi0jmHjUeCJmR9GjNpfg9J2nanAS-d94",
+    authDomain: "project-x-a9bec.firebaseapp.com",
+    projectId: "project-x-a9bec",
+    storageBucket: "project-x-a9bec.appspot.com",
+    messagingSenderId: "515793947304",
+    appId: "1:515793947304:web:3cbffbddce6ebb88c5109d"
+  }
 };
 
 /*
