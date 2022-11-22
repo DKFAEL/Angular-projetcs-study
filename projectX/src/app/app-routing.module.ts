260a23c39056dd894784d9de0286b44899d2ff62
DKFAEL <rafaelnunesemp@hotmail.com>
2022-11-22 14:38:34 -0300

    Auth guard

--- projectX/src/app/app-routing.module.ts
+++ projectX/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { CadastrarUsuarioComponent } from './views/cadastrar-usuario/cadastrar-usuario.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   }, 
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
