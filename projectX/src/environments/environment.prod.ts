d9a8dce74b104ef4b65d709894bcfd2399dadc9d
DKFAEL <rafaelnunesemp@hotmail.com>
2022-11-24 14:20:35 -0300

    img quebrada

--- projectX/src/environments/environment.prod.ts
+++ projectX/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+   apiKey: "AIzaSyATi0jmHjUeCJmR9GjNpfg9J2nanAS-d94",
+   authDomain: "project-x-a9bec.firebaseapp.com",
+   projectId: "project-x-a9bec",
+   storageBucket: "project-x-a9bec.appspot.com",
+   messagingSenderId: "515793947304",
+   appId: "1:515793947304:web:3cbffbddce6ebb88c5109d"
+ }
 };
